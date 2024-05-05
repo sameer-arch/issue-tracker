3e7ecc380222aa48ec002c5ca61a5e577612b0fc
sameer-arch <sameeralhaddad38@gmail.com>
2024-05-05 11:40:35 +0200

    Fixing cahing issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -38,6 +38,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
 			if (issue) await axios.patch("/api/issues/" + issue.id, data);
 			else await axios.post("/api/issues", data);
 			router.push("/issues");
+			router.refresh();
 			// console.log(isSubmiting);
 		} catch (error) {
 			setSubmiting(false);

fffe82bd6db962c4d15012ffca38bef0e418adc1
sameer-arch <sameeralhaddad38@gmail.com>
2024-05-05 12:22:55 +0200

    Improve the loading experience

--- app/issues/[id]/edit/loading.tsx
+++ app/issues/[id]/edit/loading.tsx
@@ -1,19 +1,2 @@
-import { Box, Card, Flex, Heading } from "@radix-ui/themes";
-import React from "react";
-import { Skeleton } from "@/app/components";
-const LoadingIssueDetailPage = () => {
-	return (
-		<Box>
-			<Skeleton className="max-w-xl" />
-			<Flex gap="3" my="2">
-				<Skeleton width="5rem" />
-				<Skeleton width="8rem" />
-			</Flex>
-			<Card mt="4" className="prose">
-				<Skeleton count={3} />
-			</Card>
-		</Box>
-	);
-};
-
-export default LoadingIssueDetailPage;
+import IssueFormSkeleton from "../../_components/IssueFormSkeleton";
+export default IssueFormSkeleton;
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -5,19 +5,15 @@ import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 import { IssueSchema } from "@/app/validationSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Issue } from "@prisma/client";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { RxInfoCircled } from "react-icons/rx";
+import SimpleMDE from "react-simplemde-editor";
 import { z } from "zod";
-import { Issue } from "@prisma/client";
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-	ssr: false,
-});
 type IssueFormData = z.infer<typeof IssueSchema>;
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {

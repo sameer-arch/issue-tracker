d0399b299c7cc9721b98f3d17b092bd3b27833e1
sameer-arch <sameeralhaddad38@gmail.com>
2024-05-05 16:42:57 +0200

    Delete an issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,6 @@
 import { IssueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
@@ -26,4 +27,20 @@ export async function PATCH(
 		},
 	});
 	return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+	request: NextRequest,
+	{ params }: { params: { id: string } }
+) {
+	const issue = await prisma.issue.findUnique({
+		where: { id: parseInt(params.id) },
+	});
+
+	if (!issue)
+		return NextResponse.json({ error: "invalid error" }, { status: 404 });
+
+	await prisma.issue.delete({ where: { id: issue.id } });
+
+	return NextResponse.json({});
 }

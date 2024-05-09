f8843d27f7817dd8851ceea943c76b1c1effa543
sameer-arch <sameeralhaddad38@gmail.com>
2024-05-09 14:24:29 +0200

    Add the login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,17 +1,19 @@
 "use client";
+import { Box } from "@radix-ui/themes";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
-
 const NavBar = () => {
+	const currentPath = usePathname();
+	const { status, data: session } = useSession();
 	const links = [
 		{ href: "/", label: "Dashboard" },
 		{ href: "/issues/list", label: "Issues" },
 	];
 
-	const currentPath = usePathname();
 	return (
 		<nav className="flex mb-5 space-x-6 border-b h-14 items-center px-5">
 			<Link href="/">
@@ -19,22 +21,31 @@ const NavBar = () => {
 			</Link>
 			<ul className="flex space-x-6">
 				{links.map((link) => (
-					<Link
-						key={link.href}
-						href={link.href}
-						className={classNames({
-							"text-zinc-900": currentPath === link.href,
-							"text-zinc-500": currentPath !== link.href,
-							"hover:text-zinc-800 transition-colors": true,
-						})}
-						// {`${
-						// currentPath === link.href ? "text-zinc-900" : "text-zinc-500"
-						// } hover:text-zinc-800 transition-colors`}
-					>
-						{link.label}
-					</Link>
+					<li key={link.href}>
+						<Link
+							href={link.href}
+							className={classNames({
+								"text-zinc-900": currentPath === link.href,
+								"text-zinc-500": currentPath !== link.href,
+								"hover:text-zinc-800 transition-colors": true,
+							})}
+							// {`${
+							// currentPath === link.href ? "text-zinc-900" : "text-zinc-500"
+							// } hover:text-zinc-800 transition-colors`}
+						>
+							{link.label}
+						</Link>
+					</li>
 				))}
 			</ul>
+			<Box>
+				{status === "authenticated" && (
+					<Link href="/api/auth/signout">Log out</Link>
+				)}
+				{status === "unauthenticated" && (
+					<Link href="/api/auth/signin">Login</Link>
+				)}
+			</Box>
 		</nav>
 	);
 };
